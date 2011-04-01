9aadbbc7e76d4f865cbb172338565ac709ceae65
Dennis Hall <halldb02@hotmail.com>
2011-04-01 18:36:01 -0400

    updated server.js and layout.js to incorporate google analytics and socket.io.port single points of definition

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-//setup Dependencies
-require(__dirname + "/lib/setup").ext( __dirname + "/lib").ext( __dirname + "/lib/express/support");
-var connect = require('connect')
-    , express = require('express')
-    , sys = require('sys')
-    , io = require('Socket.IO-node')
-    , port = (process.env.PORT || 8081);
+var googleAnalyticsSiteId = 'UA-XXXXX-X';
 
-//Setup Express
+// resolve dependencies
+require(__dirname + "/lib/setup")
+   .ext(__dirname + "/lib")
+   .ext(__dirname + "/lib/express/support");
+// end require block
+
+var connect = require('connect'),
+    express = require('express'),
+    sys = require('sys'),
+    io = require('Socket.IO-node'),
+    port = process.env.PORT || 8081;
+// end var block
+
+// set up express
 var server = express.createServer();
 server.configure(function(){
     server.set('views', __dirname + '/views');
@@ -15,42 +22,42 @@ server.configure(function(){
     server.use(server.router);
 });
 
-//setup the errors
+//set up the error handlers
 server.error(function(err, req, res, next){
-    if (err instanceof NotFound) {
-        res.render('404.ejs', { locals: { 
-                  header: '#Header#'
-                 ,footer: '#Footer#'
-                 ,title : '404 - Not Found'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX' 
-                },status: 404 });
+    if ( err instanceof NotFound ) {
+        res.render('404.ejs', {locals: {
+            header: '#Header#',
+            footer: '#Footer#',
+            title: '404 - Not Found',
+            description: '',
+            author: '',
+            googleAnalyticsSiteId: googleAnalyticsSiteId
+        }, status: 404 });
     } else {
-        res.render('500.ejs', { locals: { 
-                  header: '#Header#'
-                 ,footer: '#Footer#'
-                 ,title : 'The Server Encountered an Error'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX'
-                 ,error: err 
-                },status: 500 });
+        res.render('500.ejs', {locals: {
+            header: '#Header#',
+            footer: '#Footer#',
+            title : 'The Server Encountered an Error',
+            description: '',
+            author: '',
+            googleAnalyticsSiteId: googleAnalyticsSiteId,
+            error: err
+        }, status: 500 });
     }
 });
-server.listen( port);
+server.listen( port );
 
 //Setup Socket.IO
 var io = io.listen(server);
 io.on('connection', function(client){
-	console.log('Client Connected');
-	client.on('message', function(message){
-		client.broadcast(message);
-		client.send(message);
-	});
-	client.on('disconnect', function(){
-		console.log('Client Disconnected.');
-	});
+    console.log('Client Connected');
+    client.on('message', function(message){
+        client.broadcast(message);
+        client.send(message);
+    });
+    client.on('disconnect', function(){
+        console.log('Client Disconnected.');
+    });
 });
 
 
@@ -61,25 +68,24 @@ io.on('connection', function(client){
 /////// ADD ALL YOUR ROUTES HERE  /////////
 
 server.get('/', function(req,res){
-  res.render('index.ejs', {
-    locals : { 
-              header: '#Header#'
-             ,footer: '#Footer#'
-             ,title : 'Page Title'
-             ,description: 'Page Description'
-             ,author: 'Your Name'
-             ,analyticssiteid: 'XXXXXXX' 
-            }
-  });
+    res.render('index.ejs', {locals: {
+        header: '#Header#',
+        footer: '#Footer#',
+        title : 'Page Title',
+        description: 'Page Description',
+        author: 'Your Name',
+        googleAnalyticsSiteId: googleAnalyticsSiteId,
+        port: port
+    }});
 });
 
 
-//A Route for Creating a 500 Error (Useful to keep around)
+// a route for creating a 500 error (useful to keep around)
 server.get('/500', function(req, res){
     throw new Error('This is a 500 Error');
 });
 
-//The 404 Route (ALWAYS Keep this as the last route)
+// the 404 route (ALWAYS keep this as the last route)
 server.get('/*', function(req, res){
     throw new NotFound;
 });
@@ -91,4 +97,4 @@ function NotFound(msg){
 }
 
 
-console.log('Listening on http://0.0.0.0:' + port );
+console.log('Listening on http://0.0.0.0:' + port);

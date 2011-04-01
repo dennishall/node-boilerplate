030d230b804f2d2335e378595db2fb29acce9a5a
dennishall <halldb02@hotmail.com>
2011-04-01 10:19:47 -0700

    + copy the lib folder from html5-boilerplate

--- bin/initproject.sh
+++ bin/initproject.sh
@@ -6,6 +6,7 @@ git submodule update --init --recursive
 echo "Copying Markup and CSS BoilerPlate..."
 cp ./lib/html5-boilerplate/404.html ./views/404.ejs
 cp ./lib/html5-boilerplate/js/plugins.js ./static/js/plugins.js
+cp ./lib/html5-boilerplate/js/lib/* ./static/js/lib/
 cp ./lib/html5-boilerplate/css/handheld.css ./static/css/handheld.css
 cp ./lib/html5-boilerplate/css/style.css ./static/css/style.css
 cp ./lib/html5-boilerplate/robots.txt ./static/robots.txt
